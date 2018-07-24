8ded7799c3adbb29efd2289c8c5ce58f528b23ff
Samuel Maffei <sammaffei@mac.com>
2018-07-24 14:16:58 -0400

    Fixed problem with size classes.

--- CodingExercise/Swift/MasterViewController.swift
+++ CodingExercise/Swift/MasterViewController.swift
@@ -16,6 +16,18 @@ class MasterViewController : UIViewController
     
     var defaultModeSelectorHeight : CGFloat = 0.0
     
+    var compactInAnyDimension : Bool
+        {
+        get
+            {
+            guard let splitPlaneVC = self.splitViewController   // if we can't get it assume we do
+                else {return true}
+                
+            return (splitPlaneVC.traitCollection.horizontalSizeClass == .compact) ||
+                    (splitPlaneVC.traitCollection.verticalSizeClass == .compact)
+            }
+        }
+    
     @IBAction func performModeSwitch(_ sender : UISegmentedControl)
         {
         guard let collectionVC = childCollectionVC
@@ -65,14 +77,11 @@ class MasterViewController : UIViewController
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?)
         {
-        guard let splitPlaneVC = self.splitViewController
-            else {return}
-            
         // OK, we really shouldn't be talking iPhone vs. iPad these days. We really should be supporting
         // size classes where we can. So, the spec really only wants the mode selector to be visible
         // when the size class is compact. Otherwise, it always only text
             
-        setModeSelectorVisibilty(visible: splitPlaneVC.traitCollection.horizontalSizeClass == .compact)
+        setModeSelectorVisibilty(visible: self.compactInAnyDimension)
         }
     
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation)
